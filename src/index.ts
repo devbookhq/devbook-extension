5a1539d700d34a04001bcb1fc05c42b7c9f6063f
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-02-07 12:44:30 +0100

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './call';
 export * from './message';
 export * from './source';
 export * from './status';
+export { ExtensionExports } from './ExtensionModuleHandler';
 
 export default require.main === module && new ExtensionProcess();
