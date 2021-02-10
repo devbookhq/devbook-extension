82a61c4eda1e5048dec64fa120560c131a328606
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-02-10 18:40:37 +0100

    Fix extension endpoint params

--- src/extensionEndpoints.ts
+++ src/extensionEndpoints.ts
@@ -3,12 +3,11 @@ import axios from 'axios';
 const extensionID = process.env.EXTENSION_ID;
 
 export async function searchExtensionData(query: string, options: { indexes: string[], page?: number }) {
-  const result = await axios.post(`https://api.usedevbook.com/extensions/${extensionID}/entry/query`, {
+  const result = await axios.post(`https://api.usedevbook.com/v1/extensions/${extensionID}/entry/query`, {
     query,
     ...options.indexes,
   }, {
     params: {
-      env: process.env.EXTENSION_MODE,
       page: options.page || 10,
     },
   });
